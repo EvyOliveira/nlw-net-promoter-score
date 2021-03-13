64fd3c8605209f7aa2ba18e934905a77b31acb8b
EvyOliveira <cristinioliveira.eo@gmail.com>
2021-03-13 04:09:07 -0300

    Feat route users and userController instance

--- src/server.ts
+++ src/server.ts
@@ -4,15 +4,6 @@ import './database';
 
 const app = express();
 
-//http://localhost:3333/users
-app.get("/users", (request, response) => {
-  return response.json({message: "Hello World - NLW #4!"})
-});
 
-// 1º parâmetro: rota (recurso API)
-// 2º parâmetro: resquest, response
-app.post("/", (request, response) => {
-  return response.json({message: "Os dados foram salvos com sucesso!"})
-});
 
 app.listen(3333, () => console.log("Server is running!")); 
