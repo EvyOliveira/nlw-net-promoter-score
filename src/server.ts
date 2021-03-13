fa1939c8e6049c7962bd01d2063b54c52467950e
EvyOliveira <cristinioliveira.eo@gmail.com>
2021-03-13 02:14:17 -0300

    Feat route with the POST method

--- src/server.ts
+++ src/server.ts
@@ -7,4 +7,10 @@ app.get("/users", (request, response) => {
   return response.json({message: "Hello World - NLW #4!"})
 });
 
+// 1º parâmetro: rota (recurso API)
+// 2º parâmetro: resquest, response
+app.post("/", (request, response) => {
+  return response.json({message: "Os dados foram salvos com sucesso!"})
+});
+
 app.listen(3333, () => console.log("Server is running!")); 
