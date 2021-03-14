74d1b1728dfb53dbaabe2e9f6c84fc715b1d70bc
EvyOliveira <cristinioliveira.eo@gmail.com>
2021-03-14 00:47:46 -0300

    Feat SurveyRepository inherit User Repository attributes

---
+++ src/repositories/SurveysRepository.ts
@@ -0,0 +1,8 @@
+import { Repository, EntityRepository } from "typeorm";
+import { User } from "../models/User";
+import { Survey } from "../models/Survey";
+
+@EntityRepository(Survey)
+class SurveysRepository extends Repository<User> {}
+
+export { SurveysRepository }
