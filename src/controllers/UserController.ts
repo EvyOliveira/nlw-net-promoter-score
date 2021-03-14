f05396bf55f2bc2d995a63d09bc39bc12c430aea
EvyOliveira <cristinioliveira.eo@gmail.com>
2021-03-13 22:20:59 -0300

    Refactor controller with UsersRepository

--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -1,6 +1,6 @@
 import { Request, Response } from "express";
-import { getRepository } from "typeorm";
-import { User } from "../models/User";
+import { getCustomRepository } from "typeorm";
+import { UsersRepository } from "../repositories/UsersRepository";
 
 class UserController {
 
@@ -9,7 +9,7 @@ class UserController {
 
     const { name, email } = request.body;
 
-    const usersRepository = getRepository(User);
+    const usersRepository = getCustomRepository(UsersRepository);
     // SELECT * FROM USERS WHERE EMAIL = EMAIL
     const userAlreadyExists = await usersRepository.findOne({
       email
@@ -31,4 +31,4 @@ class UserController {
   }
 }
 
-export { UserController }
+export { UserController };
