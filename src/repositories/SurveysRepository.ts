f13e8a376eef0e25f5530ec18c622826c33b3d1e
EvyOliveira <cristinioliveira.eo@gmail.com>
2021-03-18 15:38:38 -0300

    Refactor error handling in SurveyRepository

--- src/repositories/SurveysRepository.ts
+++ src/repositories/SurveysRepository.ts
@@ -3,6 +3,10 @@ import { User } from "../models/User";
 import { Survey } from "../models/Survey";
 
 @EntityRepository(Survey)
-class SurveysRepository extends Repository<User> {}
+class SurveysRepository extends Repository<User> {
+  static find() {
+    throw new Error("Method not implemented.");
+  }
+}
 
 export { SurveysRepository }
