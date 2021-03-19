c0cba8a19159089652bb2aa1e75f95c54f33adfe
EvyOliveira <cristinioliveira.eo@gmail.com>
2021-03-18 21:03:28 -0300

    Feat import from CreateConnection for database connection

--- src/app.ts
+++ src/app.ts
@@ -2,7 +2,9 @@ import express from "express";
 import 'reflect-metadata';
 import './database';
 import { router } from "./routes";
+import createConnection from "./database";
 
+createConnection();
 const app = express();
 
 app.use(express.json());
