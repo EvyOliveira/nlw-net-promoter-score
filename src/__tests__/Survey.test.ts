c28d64d8d4455585abbda9c792f3b12525931a99
EvyOliveira <cristinioliveira.eo@gmail.com>
2021-03-22 11:12:55 -0300

    Fix syntax errors in Survey integration testing

--- src/__tests__/Survey.test.ts
+++ src/__tests__/Survey.test.ts
@@ -14,7 +14,6 @@ describe("Surveys", () => {
       title: "Title Example",
       description: "Description Example",
     });
-
     expect(response.status).toBe(201);
     expect(response.body).toHaveProperty("id");
   });
@@ -24,7 +23,6 @@ describe("Surveys", () => {
       title: "Title Example2",
       description: "Description Example2",
     });
-
     const response = await request(app).get("/surveys");
     expect(response.body.length).toBe(2);
   });
